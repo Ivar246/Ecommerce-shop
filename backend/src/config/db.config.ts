ba67234ca782adfe441b45fb08628dc5b2d5604b
Ravi Shrestha <ravistha869@gmail.com>
2024-08-31 11:40:13 +0545

    configure app and db env variable

---
+++ backend/src/config/db.config.ts
@@ -0,0 +1,12 @@
+import * as dotenv from "dotenv"
+import { DbConfig } from "../types"
+
+dotenv.config()
+
+export const dbConfig: DbConfig = {
+    DB_USER: process.env.DB_USER,
+    DB_PASS: process.env.DB_PASS,
+    DB_PORT: +process.env.DB_PORT || 5432,
+    DB_NAME: process.env.DB_NAME || "task1",
+    DB_TYPE: process.env.DB_type || "postgres"
+}
