9b6eb4ff022e4f21fd49a3cd60859ce7db2f22f9
Ravi Shrestha <ravistha869@gmail.com>
2024-09-07 23:48:30 +0545

    add Auditlog table to store audit log detail

---
+++ backend/src/entity/Auditlog.ts
@@ -0,0 +1,41 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+import { AuditLogAction, LogType } from '../enums';
+
+
+@Entity()
+export class AuditLog {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        type: "enum",
+        enum: AuditLogAction,
+        default: AuditLogAction.CREATE
+    })
+    actionType: AuditLogAction
+
+    @Column({
+        type: "enum",
+        enum: LogType,
+        default: LogType.INFO
+    })
+    logType: LogType
+
+    @Column("varchar")
+    user: string;
+
+    @Column({ type: "varchar" })
+    user_email: string
+
+    @Column("varchar")
+    module: string;
+
+    @Column('text')
+    message: string;
+
+    @Column({ type: "varchar" })
+    ip_address: string
+
+    @CreateDateColumn()
+    datetime: Date;
+}
